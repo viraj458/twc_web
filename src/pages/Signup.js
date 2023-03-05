47bfe260795ef9862f85e0b1e351ea99d64ee4c4
Viraj <virajmalaka94@gmail.com>
2023-03-05 18:27:03 +0530

    created Signup.js

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,8 +1,46 @@
-import React from 'react'
+import { useState } from "react"
+import { Link } from "react-router-dom"
 
 const Signup = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+
+const handleSubmit = async(e) => {
+  e.preventDefault()
+
+  if(password !== confirmPassword){
+    throw Error("Passwords does not match")
+  }
+  console.log(email, password, confirmPassword);
+}
+
   return (
-    <div>Signup</div>
+    <form className="signup" onSubmit={handleSubmit}>
+      <h3>Register Now!</h3>
+      
+      <input 
+        type="email" 
+        onChange={(e) => setEmail(e.target.value)} 
+        value={email} 
+        placeholder='email'
+      />
+      <input 
+        type="password" 
+        onChange={(e) => setPassword(e.target.value)} 
+        value={password} 
+        placeholder='password'
+      />
+      <input 
+        type="password" 
+        onChange={(e) => setConfirmPassword(e.target.value)} 
+        value={confirmPassword} 
+        placeholder='confirm password'
+      />
+
+      <button>register</button>
+      <Link to='/login'>back to login</Link>
+    </form>
   )
 }
 
