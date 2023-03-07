1cfedac781c23094c3ac137b51d84d2dfa3643e7
Viraj <virajmalaka94@gmail.com>
2023-03-07 15:45:16 +0530

    modified AddContact page

--- src/pages/AddContact.js
+++ src/pages/AddContact.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import LogoWhite from '../components/LogoWhite'
 import { useAuthContext } from '../hooks/useAuthContext' 
+import {BiLogOutCircle} from 'react-icons/bi'
 
 const AddContact = () => {
 
@@ -45,50 +47,74 @@ const AddContact = () => {
      
   }
   return (
-    <div>
-      <form className='form' onSubmit={handleSubmit}>
+    <div className="image w-screen h-screen flex items-center justify-center">
+      <div className='absolute w-[1530px] h-[980px] bg-[#083F46] rounded-full rotate-[20deg]'>
+        <div className="justify-center ml-[300px] mt-[200px] rotate-[-20deg]">
+            <LogoWhite/>
+          <div className='mt-14'>
+              <div>
+                <p className='font-bold text-3xl text-white mb-8'>New Contact</p>
+              </div>
+              <div>
+                <form onSubmit={handleSubmit}>
+                  <div>
+                    <input 
+                      className='w-[420px]  rounded-full mb-6 px-5 py-2 text-[18px] text-[#ff2f2f] placeholder-[#083f46] font-semibold'
+                      type="text"
+                      onChange={(e)=>{setFullName(e.target.value)}} 
+                      value={fullName}
+                      placeholder="full name"
+                    />
+                    <input 
+                      className='w-[420px] ml-10 rounded-full mb-6 px-5 py-2 text-[18px] text-[#ff2f2f] placeholder-[#083f46] font-semibold'
+                      type="text"
+                      onChange={(e)=>{setEmail(e.target.value)}} 
+                      value={email}
+                      placeholder="email"
+                    />
+                  </div>
+                  
+                  <div className='flex'>
+                    <input 
+                      className='w-[420px] rounded-full mb-6 px-5 py-2 text-[18px] text-[#ff2f2f] placeholder-[#083f46] font-semibold'
+                      type="text"
+                      onChange={(e)=>{setPhoneNumber(e.target.value)}} 
+                      value={phoneNumber}
+                      placeholder="phone number"
+                    />
+                    <div>
+                      <label className='font-semibold text-white ml-11'> gender
+                      
+                      <input 
+                      className='ml-28 mr-3'
+                      type="radio"
+                      name='gender'
+                      onChange={(e)=>{setGender(e.target.value)}} 
+                      value="Male"
+                      />male
 
-        <h2>New Contact</h2>
-
-        <input 
-          type="text"
-          onChange={(e)=>{setFullName(e.target.value)}} 
-          value={fullName}
-          placeholder="full name"
-        />
-
-        <input 
-          type="text"
-          onChange={(e)=>{setPhoneNumber(e.target.value)}} 
-          value={phoneNumber}
-          placeholder="phone number"
-        />
-
-        <input 
-          type="text"
-          onChange={(e)=>{setEmail(e.target.value)}} 
-          value={email}
-          placeholder="email"
-        />
-
-        <label> gender
-        <input 
-        type="radio"
-        name='gender'
-        onChange={(e)=>{setGender(e.target.value)}} 
-        value="Male"
-        />
-        Male
-        <input 
-        type="radio"
-        name="gender"
-        onChange={(e)=>{setGender(e.target.value)}} 
-        value="Female"
-        />
-        Female
-        </label>
-        <button>Add contact</button>
-      </form>
+                      <input 
+                      className='ml-28 mr-3'
+                      type="radio"
+                      name="gender"
+                      onChange={(e)=>{setGender(e.target.value)}} 
+                      value="Female"
+                      />female
+                      
+                      </label>
+                    </div>
+                  </div> 
+                  <button className='mt-4 border-[1px] rounded-full px-5 py-1 text-[18px] border-white font-semibold text-white'>Add contact</button>
+                </form>
+              </div>
+            
+          </div>
+          <div>
+            <button className='flex ml-[900px] mt-20 py-1 text-[18px] text-white font-bold underline underline-offset-2'><BiLogOutCircle className='w-7 h-7 mr-2'/>logout</button>
+          </div>
+        </div>
+      </div>
+      
     </div>
   )
 }
